65bc357e5d45c1d6c82efbba27bd717a1ead67a5
Shani Kehati <56911506+shanike@users.noreply.github.com>
2025-05-07 12:32:39 +0300

    chore(eslint): added fast-refresh

--- apps/client/eslint.config.mjs
+++ apps/client/eslint.config.mjs
@@ -1,8 +1,10 @@
 import nx from '@nx/eslint-plugin';
 import baseConfig from '../../eslint.config.mjs';
+import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
   ...baseConfig,
+  reactRefresh.configs.vite,
   ...nx.configs['flat/react'],
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
@@ -11,6 +13,10 @@ export default [
       '@typescript-eslint/no-empty-interface': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
       'no-console': ['warn', { allow: ['warn', 'error'] }],
+      "react-refresh/only-export-components": [
+        "error",
+        { allowConstantExport: true },
+      ],
     },
   },
 ];
