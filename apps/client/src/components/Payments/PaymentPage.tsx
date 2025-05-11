33fad8c4c46ffad9fa818e6c50e11f9874034ed4
noammosh <noammosh@mobileye.com>
2025-05-11 21:05:15 +0300

    splited expense components and added title

--- apps/client/src/components/Payments/PaymentPage.tsx
+++ apps/client/src/components/Payments/PaymentPage.tsx
@@ -1,9 +1,12 @@
-import { Stack } from "@chakra-ui/react"
+import { Box, Flex, Stack, Text } from "@chakra-ui/react"
 import BalanceCard from "./balanceCard";
 import Expenses from "./expenses";
 import CreateExpenseButton from "./createExpenseButton";
+import { useTranslation } from "react-i18next";
 
 const PaymentPage = () => {
+
+    const { t } = useTranslation();
 
     return (
         <Stack
@@ -13,9 +16,22 @@ const PaymentPage = () => {
             direction="row"
             wrap="wrap"
         >
-            <BalanceCard />
-            <CreateExpenseButton />
-            <Expenses />
+            <Box divideY={"1px"} width={"full"}>
+                <Box pb={6}>
+                    <BalanceCard />
+                </Box>
+                <Box pt={6}>
+                    <Flex width={"full"} justifyContent={"space-between"} alignItems={"baseline"} mb={2}>
+                        <Text fontSize="xl" fontWeight="bold">
+                            {t('payments.expense.all-expenses')}
+                        </Text>
+                        <CreateExpenseButton />
+                    </Flex>
+                    <Expenses />
+                </Box>
+
+            </Box>
+
         </Stack>
     )
 }
--- apps/client/src/components/Payments/expenses.tsx
+++ apps/client/src/components/Payments/expenses.tsx
@@ -1,22 +1,17 @@
-import { Box, Card, Flex, For, Icon, SkeletonText, Stack, Text } from "@chakra-ui/react";
-import { useNavigate } from "@tanstack/react-router";
+import { Box, Flex, For, SkeletonText, Stack, Text } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
-import { LuChevronLeft } from "react-icons/lu";
+import { ApartmentExpense } from "../../api/apartmentExpenses";
 import { useAuth } from "../../context/auth/AuthProvider";
 import { useApartmentExpenses } from "../../hooks/useApartmentExpenses";
-import { useLocaleChange } from "../../hooks/useLocaleChange";
-import { roundUpToXDigits } from "../../utilities/roundUpToXDigits";
+import ExpenseCard from "./expenseCard";
 
 const Expenses = () => {
 
     const { t } = useTranslation();
     const { currentUserDetails } = useAuth();
-
     const { data, isLoading, isError } = useApartmentExpenses(currentUserDetails!.userId);
-    const navigate = useNavigate();
-    const { isRTL } = useLocaleChange();
 
-    const dataPerMonth = data?.apartmentExpenses.reduce((acc: { [key: string]: any[] }, item) => {
+    const dataPerMonth = data?.apartmentExpenses.reduce((acc: { [key: string]: ApartmentExpense[] }, item) => {
         const month = new Date(item.expense_createdAt).toLocaleString('default', { month: 'numeric', year: 'numeric' });
         if (!acc[month]) {
             acc[month] = [];
@@ -37,52 +32,12 @@ const Expenses = () => {
                     <For each={Object.entries(dataPerMonth)} fallback={<SkeletonText noOfLines={1} width="50%" mb="2" />}>
                         {([month, expenses]) => (
                             <Box key={month} mb={6}>
-                                <Box fontSize="l" fontWeight="bold" mb={2}>
+                                <Box fontSize="l" mb={2} color={"gray.600"}>
                                     {(t(`months.${month.split("/")[0]}` as any) as string)} {month.split('/')[1]}
                                 </Box>
                                 <Stack gap={3}>
                                     <For each={expenses}>
-                                        {(item) => (
-                                            <Card.Root
-                                                cursor={"pointer"}
-                                                onClick={() => navigate({ to: `/roommate/payments/expenses/${item.expense_expenseId}` })}
-                                                width="100%"
-                                                key={item.expense_expenseId}
-                                            >
-                                                <Card.Body p={4}>
-                                                    <Flex gap="3" alignItems="center" justifyContent="space-between">
-                                                        <Flex direction="column" flexGrow={1}>
-                                                            <Card.Title>{item.expense_description}</Card.Title>
-                                                            <Card.Description as="div">
-                                                                {item.paidByMe
-                                                                    ? t("payments.you-paid", { amount: roundUpToXDigits(item.expense_amount) })
-                                                                    : t("payments.paid-by", {
-                                                                        amount: roundUpToXDigits(item.expense_amount),
-                                                                        name: `${item.paidByFirstName} ${item.paidByLastName[0]}`,
-                                                                    })}
-                                                            </Card.Description>
-                                                        </Flex>
-                                                        <Card.Description
-                                                            as="div"
-                                                            whiteSpace="pre-line"
-                                                            textAlign="end"
-                                                            color={item.paidByMe ? "green.600" : "red.600"}
-                                                        >
-                                                            {item.paidByMe
-                                                                ? t("payments.you-lent", { amount: roundUpToXDigits(item.expense_amount - Number(item.splitAmount)) })
-                                                                : t("payments.you-borrowed", { amount: roundUpToXDigits(item.splitAmount) })
-                                                            }
-                                                        </Card.Description>
-                                                        {/* <IconButton variant={"surface"} onClick={() => navigate({ to: `/roommate/payments/expenses/${item.expense_expenseId}` })} aria-label="Edit" size="sm" colorScheme="blue">
-                                                        <LuPencil />
-                                                    </IconButton> */}
-                                                        <Icon rotate={isRTL ? "unset" : "180deg"}>
-                                                            <LuChevronLeft />
-                                                        </Icon>
-                                                    </Flex>
-                                                </Card.Body>
-                                            </Card.Root>
-                                        )}
+                                        {(item) => <ExpenseCard item={item} />}
                                     </For>
                                 </Stack>
                             </Box>
