008f8ecb75867824237bf2fdf2c586ed2d9014b4
Shani Kehati <56911506+shanike@users.noreply.github.com>
2025-05-12 09:49:34 +0300

    feat(CreateApartment): apply the `ApartmentLayout` with "go back" functionality

--- apps/client/src/pages/CreateApartment/index.tsx
+++ apps/client/src/pages/CreateApartment/index.tsx
@@ -1,57 +1,21 @@
+import { useNavigate } from '@tanstack/react-router';
+import { useState, type FC } from 'react';
 import { ApartmentInfo } from './ApartmentInfo';
-import { Apartment } from '../../../../server/src/apartment/apartment.entity';
-import React, { useCallback, useEffect, useState } from 'react';
-import { useNavigate, useSearch } from '@tanstack/react-router';
-import { useAuth } from '../../context/auth/AuthProvider';
+import ApartmentLayout from './ApartmentLayout';
 import ApartmentSettings from './ApartmentSettings';
 import RenterSettings from './RenterSettings';
 import { ApartmentInfoDto, UserRoleName } from '@komuna/types';
 
 enum CreateApartmentPages {
-  ApartmentInfo,
-  ApartmentSettings,
-  RenterSettings,
+  ApartmentInfo = 1,
+  ApartmentSettings = 2,
+  RenterSettings = 3,
 }
 
-export const CreateApartment = () => {
-  const [page, setPage] = useState<CreateApartmentPages>(CreateApartmentPages.ApartmentInfo);
-
-  const [apartmentInfo, setApartmentInfo] = useState<ApartmentInfoDto>({
-    name: '',
-    address: '',
-    city: '',
-    role: UserRoleName.Renter
-  });
-
-  // const [profileDetatils, setProfileDetails] = useState<CreateUserDto>({
-  //   firstName: '',
-  //   lastName: '',
-  //   image: '/meerkats/waving.png',
-  //   phoneNumber,
-  // });
-
-  // const [profileDetatils, setProfileDetails] = useState<CreateUserDto>({
-  //   firstName: '',
-  //   lastName: '',
-  //   image: '/meerkats/waving.png',
-  //   phoneNumber,
-  // });
-
-  const navigate = useNavigate();
-
-  const goBack = useCallback(() => {
-    setPage((oldPage) => {
-      if (oldPage > 0) {
-        return oldPage - 1;
-      } else {
-        navigate({ to: '/select-apartment' });
-        return oldPage;
-      }
-    });
-  }, [setPage]);
-
-
-
+interface CreateApartmentFormProps {
+  page: CreateApartmentPages;
+}
+const CreateApartmentForm: FC<CreateApartmentFormProps> = ({ page }) => {
   switch (page) {
     case CreateApartmentPages.ApartmentInfo:
       return (
@@ -74,4 +38,78 @@ export const CreateApartment = () => {
   }
 };
 
-export default CreateApartment;
+/**
+ * Wraps the CreateApartmentForm in a layout with a back button.
+ */
+export const CreateApartment = () => {
+  const [page, setPage] = useState<CreateApartmentPages>(CreateApartmentPages.ApartmentInfo);
+  const navigate = useNavigate();
+
+  /**
+   * Goes back one page, unless on the first page, in which case it navigates to the select apartment page.
+   */
+  const goPageBack = (currentPage: CreateApartmentPages) => {
+    if (currentPage === CreateApartmentPages.ApartmentInfo) {
+      navigate({ to: '/select-apartment' });
+    } else {
+      setPage((prevPage) => prevPage - 1);
+    }
+  };
+
+  return (
+    <ApartmentLayout button="back" onButtonClick={() => goPageBack(page)}>
+      <CreateApartmentForm page={page} />
+    </ApartmentLayout>
+  );
+};
+
+// export const CreateApartment = () => {
+//   const { refetchAuth, currentUserDetails } = useAuth();
+//   const [phoneNumber, setPhoneNumber] = React.useState('');
+//   const navigate = useNavigate();
+//   const search = useSearch({ from: '/login' });
+//   const [loginStep, setLoginStep] = useState<LoginSteps>('enter_phone');
+
+//   const onSendPincodeSuccess = (phone: string) => {
+//     setPhoneNumber(phone);
+//     setLoginStep('verify_pincode');
+//   };
+
+//   const onPincodeSuccess = async (isUser: boolean) => {
+//     if (isUser) {
+//       await refetchAuth?.();
+//     } else {
+//       setLoginStep('create_profile');
+//     }
+//   };
+
+//   const onUserCreatedSuccessfully = async () => {
+//     await refetchAuth?.();
+//   };
+
+//   useEffect(() => {
+//     if (currentUserDetails) {
+//       navigate({
+//         to: search.redirect ?? '/select-apartment',
+//         replace: true,
+//       });
+//     }
+//   }, [currentUserDetails, navigate, search.redirect]);
+
+//   switch (loginStep) {
+//     case 'enter_phone':
+//       return <EnterPhoneStep onSendPincodeSuccess={onSendPincodeSuccess} />;
+//     case 'verify_pincode':
+//       return (
+//         <VerifyPincodeStep
+//           goBack={() => setLoginStep('enter_phone')}
+//           phoneNumber={phoneNumber}
+//           onPincodeSuccess={onPincodeSuccess}
+//         />
+//       );
+//     case 'create_profile':
+//       return <CreateProfileStep phoneNumber={phoneNumber} onUserCreatedSuccessfully={onUserCreatedSuccessfully} />;
+//     default:
+//       return null;
+//   }
+// };
