720e5da17f35a43862103f6321190ed56342df84
Shani Kehati <56911506+shanike@users.noreply.github.com>
2025-05-12 09:41:28 +0300

    feat(ApartmentLayout): support a "back" button in addition to a "logout" button

--- apps/client/src/pages/CreateApartment/ApartmentLayout.tsx
+++ apps/client/src/pages/CreateApartment/ApartmentLayout.tsx
@@ -1,13 +1,24 @@
-import { Box, HStack, VStack } from "@chakra-ui/react"
-import { ReactNode } from "@tanstack/react-router"
-import { LogoutButton } from "../../components/LogoutButton"
+import { Box, HStack, VStack } from "@chakra-ui/react";
+import type { PropsWithChildren } from "react";
+import { BackNavigationBar } from "../../components/BackNavigationBar";
+import { LogoutButton } from "../../components/LogoutButton";
 
-export const ApartmentLayout = ({ children }: ReactNode) => {
+type Buttons = "back" | "logout";
+interface ApartmentLayoutProps extends PropsWithChildren<{}> {
+  button?: Buttons;
+  onButtonClick?: () => void;
+}
+
+export const ApartmentLayout = ({ children, button = "logout", onButtonClick }: ApartmentLayoutProps) => {
   return (
     <Box backgroundColor="brand.500" flex="1" display="flex" flexDirection="column" gap="0">
-      <HStack justifyContent="end" paddingX="2" paddingY="1">
-        <LogoutButton />
-      </HStack>
+      {button === "back" ? (
+        <BackNavigationBar onGoBack={onButtonClick} />
+      ) : (
+        <HStack justifyContent="end" paddingX="2" paddingY="1">
+          <LogoutButton />
+        </HStack>
+      )}
       <VStack
         paddingY="44px"
         paddingX="25px"
@@ -22,6 +33,6 @@ export const ApartmentLayout = ({ children }: ReactNode) => {
       </VStack>
     </Box>
   );
-}
+};
 
 export default ApartmentLayout;
