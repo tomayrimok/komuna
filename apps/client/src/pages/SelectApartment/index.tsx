f1fe3951de778362578da005cc49f7616d1eba8e
Adi Ella Pinto <adipintodev@gmail.com>
2025-05-12 23:32:05 +0300

    added the select apartment - the right one

--- apps/client/src/pages/SelectApartment/index.tsx
+++ apps/client/src/pages/SelectApartment/index.tsx
@@ -1,47 +1,57 @@
-import { Box, Button, HStack, Image, Text, VStack } from '@chakra-ui/react';
+import { Button, Image, Spacer, Stack, Text, VStack } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
-import { useAuth } from '../../context/auth/AuthProvider';
-import { LogoutButton } from '../../components/LogoutButton';
 import { useNavigate } from '@tanstack/react-router';
 import ApartmentLayout from '../CreateApartment/ApartmentLayout';
+import { UserRoleName } from '@komuna/types';
+
+const apartments = [
+  { address: 'ישראל אהרוני 10', role: UserRoleName.Renter },
+  { address: 'טשרניחובסקי 50', role: UserRoleName.Leaser },
+];
 
 export const SelectApartment = () => {
-  const { currentUserDetails } = useAuth();
+
   const { t } = useTranslation();
   const navigate = useNavigate();
-
   return (
     <ApartmentLayout goBack={() => { }}>
-      <VStack>
-        <Text fontSize="2xl" fontWeight="bold">
-          {t('select_apartment.no_apartments.title')}
-        </Text>
-        <Text fontSize="lg">
-          {t('select_apartment.no_apartments.description', { firstName: currentUserDetails?.firstName })}
-        </Text>
+      <Text textAlign='center' fontSize="2xl" fontWeight="bold" >
+        {t('choose_apartment.title')}
+      </Text>
+      <VStack gap='6'>
+        {apartments.map((apartment) => {
+          return (
+            <Button
+              size="xl"
+              fontSize="2xl"
+              fontWeight="bold"
+              h="200px"
+              variant="outline"
+              backgroundColor='transparent'
+              onClick={() => { navigate({ to: '/roommate' }); }}>
+              <VStack w='100%'>
+                <Image h='60px' src="/detailed_icons/apartment.png" />
+                <Text>{apartment.address}</Text>
+                <Text>(
+                  {apartment.role === UserRoleName.Renter ?
+                    t('choose_apartment.renter')
+                    : t('choose_apartment.leaser')}
+                  )
+                </Text>
+              </VStack>
+            </Button>
+          )
+        })}
       </VStack>
-      <VStack gap="12">
-        <VStack gap="4">
-          <Image src="/detailed_icons/join_apartment.png" maxW="200px" />
-          <Button
-            size="xl"
-            fontSize="2xl"
-            fontWeight="bold"
-            onClick={() => { navigate({ to: '/join-apartment' }); }}>
-            {t('select_apartment.no_apartments.join_apartment')}
-          </Button>
-        </VStack>
-        <VStack gap="5">
-          <Image src="/detailed_icons/create_apartment.png" maxW="200px" />
-          <Button
-            size="xl"
-            fontSize="2xl"
-            fontWeight="bold"
-            onClick={() => { navigate({ to: '/create-apartment' }); }}>
-            {t('select_apartment.no_apartments.create_new_apartment')}
-          </Button>
-        </VStack>
-      </VStack>
-    </ApartmentLayout>
+      <Spacer />
+      <Button
+        size="xl"
+        fontSize="2xl"
+        fontWeight="bold"
+        backgroundColor='transparent'
+        onClick={() => { navigate({ to: '/create-apartment' }); }}>
+        + {t('select_apartment.no_apartments.create_new_apartment')}
+      </Button>
+    </ApartmentLayout >
   );
 };
