f205df859497de401a1ca39e5e02ea24ce989008
Shani Kehati <56911506+shanike@users.noreply.github.com>
2025-05-13 21:07:11 +0300

    feat: generate code on create-apartment

--- apps/server/src/apartment/apartment.controller.ts
+++ apps/server/src/apartment/apartment.controller.ts
@@ -1,21 +1,23 @@
 import { Body, Controller, Post, Get, Query } from '@nestjs/common';
 import { ApartmentService } from './apartment.service';
 import { CreateApartmentDto } from './dto/create-apartment.dto';
-import { UserApartmentService } from '../user-apartment/user-apartment.service';
 import { Apartment } from './apartment.entity';
 import { User } from '../user/user.entity';
 import { UserApartment } from '../user-apartment/user-apartment.entity';
 import { UseAuth } from '../decorators/UseAuth';
 import { User as GetUser } from '../decorators/User';
 import { UserRole } from '@komuna/types';
+import { generateVerificationCode } from '../utils/generateVerificationCode';
 
 @Controller('apartment')
 export class ApartmentController {
   constructor(private readonly apartmentService: ApartmentService) { }
+  private static readonly CODE_LENGTH = 4;
+
 
   @Post()
   @UseAuth()
-  createApartment(@Body() createApartmentData: CreateApartmentDto, @GetUser() user: User) {
+  async createApartment(@Body() createApartmentData: CreateApartmentDto, @GetUser() user: User) {
     console.log("In createApartment");
     console.log('createApartmentData: ', createApartmentData);
     console.log('user: ', user);
@@ -28,6 +30,7 @@ export class ApartmentController {
     userApartment.rent = createApartmentData.renterSettings.rent;
     userApartment.role = createApartmentData.apartmentInfo.role;
 
+    const generatedCode = generateVerificationCode(ApartmentController.CODE_LENGTH);
     const apartment = new Apartment();
     apartment.name = createApartmentData.apartmentInfo.name;
     apartment.address = createApartmentData.apartmentInfo.address;
@@ -39,12 +42,15 @@ export class ApartmentController {
     apartment.houseCommitteeRent = createApartmentData.renterSettings.houseCommitteeRent;
     apartment.houseCommitteePayerUser = houseCommitteePayerUser;
     apartment.residents = [userApartment];
+    apartment.code = generatedCode;
 
-    return this.apartmentService.createApartment(apartment);
+    await this.apartmentService.createApartment(apartment);
+    return generatedCode;
   }
 
   @Get()
     async getApartmentUsers(@Query('apartmentId') apartmentId: string) {
         return await this.apartmentService.getApartmentWithResidents(apartmentId);
     }
+
 }
