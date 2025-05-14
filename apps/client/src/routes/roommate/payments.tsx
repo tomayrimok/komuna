d900e57c9f65520b5ed34d947f01b23a70cab285
noammosh <noammosh@mobileye.com>
2025-05-14 18:03:03 +0300

    deleted unnecessary

--- apps/client/src/routes/roommate/payments.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/roommate/payments')({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/member/payments"!</div>;
-}
