f1689ee987b9184230132552720cbf3450af1c19
Shani Kehati <56911506+shanike@users.noreply.github.com>
2025-05-14 13:52:01 +0300

    feat(client): join apartment by code

--- apps/client/src/pages/JoinApartment/index.tsx
+++ apps/client/src/pages/JoinApartment/index.tsx
@@ -1,10 +1,13 @@
-import { useTranslation } from "react-i18next";
-import ApartmentLayout from "../CreateApartment/ApartmentLayout"
-import ApartmentTitle from "../CreateApartment/ApartmentTitle";
-import { Button, PinInput, Spacer } from "@chakra-ui/react";
-import { useState } from "react";
-import { times } from "lodash";
-import { useNavigate } from "@tanstack/react-router";
+import { useTranslation } from 'react-i18next';
+import ApartmentLayout from '../CreateApartment/ApartmentLayout';
+import ApartmentTitle from '../CreateApartment/ApartmentTitle';
+import { Button, PinInput, Spacer } from '@chakra-ui/react';
+import { useState } from 'react';
+import { times } from 'lodash';
+import { useNavigate } from '@tanstack/react-router';
+import axios from 'axios';
+import { toaster } from '../../chakra/ui/toaster';
+import { useMutation } from '@tanstack/react-query';
 
 const JoinApartment = () => {
   const [pincode, setPincode] = useState(['', '', '', '']);
@@ -12,13 +15,33 @@ const JoinApartment = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
+  const joinApartmentMutation = useMutation({
+    mutationFn: async (code: string) => {
+      await axios.post(`/api/apartment/join/${code}`);
+    },
+    onSuccess: () => {
+      toaster.create({
+        title: t('join_existing_apartment.success'),
+        type: 'success',
+      });
+    },
+    onError: () => {
+      toaster.create({
+        title: t('join_existing_apartment.error'),
+        type: 'error',
+      });
+    },
+  });
+
   return (
     <ApartmentLayout
-      goBack={() => { navigate({ to: '/new-apartment' }); }}
+      goBack={() => {
+        navigate({ to: '/new-apartment' });
+      }}
     >
       <ApartmentTitle
-        title={t("join_existing_apartment.title")}
-        description={t("join_existing_apartment.description")}
+        title={t('join_existing_apartment.title')}
+        description={t('join_existing_apartment.description')}
       />
       <PinInput.Root
         otp
@@ -27,7 +50,7 @@ const JoinApartment = () => {
         onValueChange={(e) => setPincode(e.value)}
         size="2xl"
         variant="outline"
-      // onValueComplete={(details) => ({ code: details.valueAsString })}
+        // onValueComplete={(details) => ({ code: details.valueAsString })}
       >
         <PinInput.HiddenInput />
         <PinInput.Control fontWeight="bold" fontSize="2xl" dir="ltr">
@@ -37,14 +60,11 @@ const JoinApartment = () => {
         </PinInput.Control>
       </PinInput.Root>
       <Spacer />
-      <Button
-        size="xl"
-        fontSize="2xl"
-        fontWeight="bold">
+      <Button size="xl" fontSize="2xl" fontWeight="bold" onClick={() => joinApartmentMutation.mutate(pincode.join(''))}>
         {t('join_existing_apartment.join_btn')}
       </Button>
     </ApartmentLayout>
   );
-}
+};
 
 export default JoinApartment;
