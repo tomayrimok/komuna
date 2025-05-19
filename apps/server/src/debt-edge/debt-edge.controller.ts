e98dc367ca91438acf63443a8a52c0d0f1e5fb3e
tomrrimok <tom.rimok@fiverr.com>
2025-05-19 20:33:14 +0300

    stage

--- apps/server/src/debt-edge/debt-edge.controller.ts
+++ apps/server/src/debt-edge/debt-edge.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { DebtEdgeService } from './debt-edge.service';
 
 @Controller('debt-edge')
-export class DebtEdgeController {}
+export class DebtEdgeController {
+    constructor(
+        private readonly debtEdgeService: DebtEdgeService,
+    ) { }
+
+    @Get('user-balance-details')
+    async getUserBalanceDetails(@Query('userId') userId: string, @Query('apartmentId') apartmentId: string) {
+        return await this.debtEdgeService.getUserBalanceDetails(apartmentId, userId);
+    }
+
+    @Get('user-balance')
+    async getUserBalance(@Query('userId') userId: string, @Query('apartmentId') apartmentId: string) {
+        return await this.debtEdgeService.getUserBalance(apartmentId, userId);
+    }
+
+    @Get('debt-details')
+    async getDebtDetails(@Query('debtId') debtId: string) {
+        return await this.debtEdgeService.getDebtDetails(debtId);
+    }
+}
