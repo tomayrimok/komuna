29f52de614162b540c66c5f29b9f9b8cade705e0
tomrrimok <tom.rimok@fiverr.com>
2025-05-19 20:23:31 +0300

    change query builder to regular

--- apps/server/src/apartment/apartment.service.ts
+++ apps/server/src/apartment/apartment.service.ts
@@ -5,39 +5,27 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class ApartmentService {
+  constructor(
+    @InjectRepository(Apartment)
+    private readonly apartmentRepo: Repository<Apartment>
+  ) {}
 
-    constructor(
-        @InjectRepository(Apartment)
-        private readonly apartmentRepo: Repository<Apartment>,
-    ) { }
+  async createApartment(apartment: Partial<Apartment>) {
+    return await this.apartmentRepo.save(apartment);
+  }
 
-    async createApartment(apartment: Partial<Apartment>) {
-        return await this.apartmentRepo.save(apartment);
-    }
+  async updateApartment(apartmentId: string, apartment: Partial<Apartment>) {
+    return await this.apartmentRepo.update({ apartmentId }, apartment);
+  }
 
-    async updateApartment(apartmentId: string, apartment: Partial<Apartment>) {
-        return await this.apartmentRepo.update({ apartmentId }, apartment);
-    }
-
-    async getApartmentWithResidents(apartmentId: string) {
-        return await this.apartmentRepo
-            .createQueryBuilder('apartment')
-            .leftJoinAndSelect('apartment.residents', 'resident')
-            .leftJoinAndSelect('resident.user', 'user')
-            .where('apartment.apartmentId = :apartmentId', { apartmentId })
-            .select([
-                'apartment.apartmentId',
-                'apartment.name',
-                'resident.userId',
-                'resident.rent',
-                'resident.role',
-                'user.userId',
-                'user.firstName',
-                'user.lastName',
-                'user.image',
-                'user.phoneNumber',
-            ])
-            .getOne();
-    }
-
+  async getApartmentWithResidents(apartmentId: string) {
+    return await this.apartmentRepo.findOne({
+      where: { apartmentId },
+      relations: {
+        residents: {
+          user: true,
+        },
+      },
+    });
+  }
 }
