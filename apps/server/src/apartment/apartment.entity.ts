71db89499e49397d28d883a76a6aebb6f01b0b3e
Shani Kehati <56911506+shanike@users.noreply.github.com>
2025-05-29 23:15:59 +0300

    refactor: reorder fields

--- apps/server/src/apartment/apartment.entity.ts
+++ apps/server/src/apartment/apartment.entity.ts
@@ -15,62 +15,59 @@ export class Apartment {
   @PrimaryGeneratedColumn('uuid')
   apartmentId: string;
 
-    /** Apartment Info */
+  /** Apartment Info */
   @Column()
   name: string;
 
   @Column({ nullable: true })
   image?: string;
 
-    /** The code to join the apartment. NULL in case the apartment doesn't allow new residents */
+  /** The code to join the apartment. NULL in case the apartment doesn't allow new residents */
   @Column({ unique: true })
   code: string;
 
-    /** Apartment Info */
+  /** Apartment Info */
   @Column({ nullable: true })
   address?: string;
 
-    /** Apartment Info */
+  /** Apartment Info */
   @Column({ nullable: true })
   city?: string;
 
-    /** Apartment Settings */
-    @Column({ type: 'date', nullable: true })
-    contractEndDate?: Date;
-
-    /** contract file UserRole */
-    @Column({ nullable: true })
-    contractUrl?: string;
-
-    /** Apartment Settings */
-    @Column({ type: 'float', nullable: true })
-    rent?: number;
-
-
-    /** בעל הבית */
   @Column({ nullable: true })
-    // TODO change to landLordId?!
+  // TODO change to landLordId?!
   managerId?: string;
 
+  /** Apartment Settings */
+  @Column({ type: 'date', nullable: true })
+  contractEndDate?: Date;
+
+  /** contract file UserRole */
   @Column({ nullable: true })
-    contract?: string;
+  contractUrl?: string;
 
-    @Column({ nullable: true })
-    billsDetails?: string;
+  @Column({ type: 'float', nullable: true })
+  rent?: number;
 
-    /** Renter Settings */
-    /** מחיר חושי ועד בית */
-    @Column({ type: 'float', nullable: true })
-    houseCommitteeRent: number;
+  @Column({ nullable: true })
+  contract?: string;
 
-    /** Renter Settings */
-    /** The user id of who pays the house committee, or NULL if it's split equally */
-    @ManyToOne(() => User, (u) => u.userId, { nullable: true })
-    @JoinColumn({ name: 'houseCommitteePayerUserId' })
-    houseCommitteePayerUser?: User;
+  @Column({ nullable: true })
+  billsDetails?: string;
 
-    @OneToMany(() => UserApartment, ua => ua.apartment, { cascade: true })
-    residents: UserApartment[];
+  /** Renter Settings */
+  /** מחיר חושי ועד בית */
+  @Column({ type: 'float', nullable: true })
+  houseCommitteeRent: number;
+
+  /** Renter Settings */
+  /** The user id of who pays the house committee, or NULL if it's split equally */
+  @ManyToOne(() => User, (u) => u.userId, { nullable: true })
+  @JoinColumn({ name: 'houseCommitteePayerUserId' })
+  houseCommitteePayerUser?: User;
+
+  @OneToMany(() => UserApartment, (ua) => ua.apartment, { cascade: true })
+  residents: UserApartment[];
 
   @OneToMany(() => Task, (task) => task.apartmentId)
   tasks: Task[];
