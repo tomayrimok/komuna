8800524a1fc87a76928fa3784ab4f4164180959b
Shani Kehati <56911506+shanike@users.noreply.github.com>
2025-05-29 23:15:50 +0300

    refactor: reorder imports

--- apps/server/src/apartment/apartment.entity.ts
+++ apps/server/src/apartment/apartment.entity.ts
@@ -1,41 +1,35 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
-import { UserApartment } from '../user-apartment/user-apartment.entity';
-import { Task } from '../task/task.entity';
-import { Expense } from '../expense/expense.entity';
-import { Payment } from '../payment/payment.entity';
-import { Incident } from '../incident/incident.entity';
-import { ShoppingTemplate } from '../shopping-template/shopping-template.entity';
-import { ShoppingList } from '../shopping-list/shopping-list.entity';
-import { ApiProperty } from '@nestjs/swagger';
-import { User } from '../user/user.entity';
 import { BillsDetails } from '@komuna/types';
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Expense } from '../expense/expense.entity';
+import { Incident } from '../incident/incident.entity';
+import { Payment } from '../payment/payment.entity';
+import { ShoppingList } from '../shopping-list/shopping-list.entity';
+import { ShoppingTemplate } from '../shopping-template/shopping-template.entity';
+import { Task } from '../task/task.entity';
+import { UserApartment } from '../user-apartment/user-apartment.entity';
+import { User } from '../user/user.entity';
 
 @Entity()
 export class Apartment {
-  @ApiProperty({ description: 'Unique identifier for the apartment' })
   @PrimaryGeneratedColumn('uuid')
   apartmentId: string;
 
-  @ApiProperty({ description: 'Name of the apartment' })
     /** Apartment Info */
   @Column()
   name: string;
 
-  @ApiProperty({ description: 'URL to apartment image', required: false })
   @Column({ nullable: true })
   image?: string;
 
-  @ApiProperty({ description: 'Unique code for the apartment' })
     /** The code to join the apartment. NULL in case the apartment doesn't allow new residents */
   @Column({ unique: true })
   code: string;
 
-  @ApiProperty({ description: 'Apartment address', required: false })
     /** Apartment Info */
   @Column({ nullable: true })
   address?: string;
 
-  @ApiProperty({ description: 'City where the apartment is located', required: false })
     /** Apartment Info */
   @Column({ nullable: true })
   city?: string;
@@ -54,16 +48,13 @@ export class Apartment {
 
 
     /** בעל הבית */
-  @ApiProperty({ description: 'ID of the apartment manager', required: false })
   @Column({ nullable: true })
     // TODO change to landLordId?!
   managerId?: string;
 
-  @ApiProperty({ description: 'Apartment contract details', required: false })  
   @Column({ nullable: true })
     contract?: string;
 
-    @ApiProperty({ description: 'Bills payment details', required: false })
     @Column({ nullable: true })
     billsDetails?: string;
 
@@ -78,35 +69,27 @@ export class Apartment {
     @JoinColumn({ name: 'houseCommitteePayerUserId' })
     houseCommitteePayerUser?: User;
 
-    @ApiProperty({ type: () => [UserApartment], description: 'Apartment residents' })
     @OneToMany(() => UserApartment, ua => ua.apartment, { cascade: true })
     residents: UserApartment[];
 
-  @ApiProperty({ type: () => [Task], description: 'Apartment tasks' })
   @OneToMany(() => Task, (task) => task.apartmentId)
   tasks: Task[];
 
-  @ApiProperty({ type: () => [Expense], description: 'Apartment expenses' })
   @OneToMany(() => Expense, (e) => e.apartmentId)
   expenses: Expense[];
 
-  @ApiProperty({ type: () => [Payment], description: 'Apartment payments' })
   @OneToMany(() => Payment, (p) => p.apartmentId)
   payments: Payment[];
 
-  @ApiProperty({ type: () => [Incident], description: 'Apartment incidents' })
   @OneToMany(() => Incident, (i) => i.apartmentId)
   incidents: Incident[];
 
-  @ApiProperty({ type: () => [ShoppingTemplate], description: 'Apartment shopping templates' })
   @OneToMany(() => ShoppingTemplate, (st) => st.apartmentId)
   shoppingTemplates: ShoppingTemplate[];
 
-  @ApiProperty({ type: () => [ShoppingList], description: 'Apartment shopping lists' })
   @OneToMany(() => ShoppingList, (sl) => sl.contextId)
   shoppingLists: ShoppingList[];
 
-  @ApiProperty({ description: 'Date when the apartment was created' })
   @CreateDateColumn()
   createdAt: Date;
 }
