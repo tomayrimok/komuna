d7c39c2df5c4e4b52efa0168e47691f90a6a04ed
noammosh <noammosh@mobileye.com>
2025-06-04 00:41:34 +0300

    expense details style

--- apps/client/src/components/Payments/expenseDetailsPage.tsx
+++ apps/client/src/components/Payments/expenseDetailsPage.tsx
@@ -38,89 +38,117 @@ const ExpenseDetailsPage = () => {
     if (expenseId && isExpenseDetailsLoading) return <Text>Loading...</Text>;
 
     return (
-        <Container maxW="lg" py={8}>
-            <Stack gap={6}>
-                <Heading size="lg" textAlign="center">
-                    {expenseId ? t("payments.expense.edit-expense") : t("payments.expense.create-expense")}
-                </Heading>
-
-                <Box p={4} borderWidth={1} borderRadius="xl" boxShadow="sm" bg="white">
-                    <Stack gap={4}>
-                        <Box>
-                            <Text mb={1} fontWeight="medium">
-                                {t("payments.expense.expense-description")}
-                            </Text>
-                            <Textarea
-                                // placeholder={t("payments.expense.expense-description")}
-                                value={expenseDetails.description}
-                                onChange={(e) => setDescription(e.target.value)}
-                                resize={"none"}
-                            />
-                        </Box>
-
-                        <Box>
-                            <Text mb={1} fontWeight="medium">
-                                {t("payments.expense.expense-amount")}
-                            </Text>
-                            <Flex alignItems="center" gap={2}>
-                                <NumberInput.Root
-                                    maxW="200px"
-                                    onValueChange={(e) => setAmount(Number(e.value))}
-                                    value={String(expenseDetails.amount)}
-                                >
-                                    <NumberInput.Control />
-                                    <NumberInput.Input placeholder="0.00" />
-                                </NumberInput.Root>
-                                <Text fontSize="xl" >
-                                    ₪
-                                </Text>
-                            </Flex>
-                        </Box>
-
-                        <hr />
-                        <Flex alignItems="center" gap={2} flexWrap="wrap">
-                            <Text>
-                                {t("payments.expense.paid-by")}
-                            </Text>
-                            <SelectUserDrawer
-                                size={"lg"}
-                                title={t("payments.expense.select-paid-by")}
-                                onSelect={setPaidBy}
-                                trigger={
-                                    <Button size="sm" px={1} py={0} h="unset" variant={"surface"}>
-                                        {expenseDetails.paidByUser?.firstName} {expenseDetails.paidByUser?.lastName}
-                                    </Button>
-                                }
-                            />
-                            <Text>
-                                {t("payments.expense.and-splits")}
-                            </Text>
-                            <SplitDetailsDrawer
-                                trigger={
-                                    <Button size="sm" px={1} py={0} h="unset" variant={"surface"}>
-                                        {areSplitsValuesEqual ? t("payments.expense.equally") : t("payments.expense.unequally")}
-                                    </Button>
-                                }
-                            />
+        <Box
+            bgImage={`url("data:image/svg+xml,%3Csvg width='100' height='100' xmlns='http://www.w3.org/2000/svg'%3E%3Ctext x='40' y='40' font-size='80' fill='rgba(252, 219, 167, 0.37)' transform='rotate(30)'%3E₪%3C/text%3E%3C/svg%3E")`}
+            bgRepeat="repeat"
+            h="full"
+            pt={7}
+        >
+            <Container maxW="lg" p={8} h="full" display="flex" flexDirection={"column"}>
+                <Stack gap={6} flexGrow={1}>
+                    <Flex flexDirection={"column"} alignItems="center" gap={4}>
+                        <Flex
+                            alignItems={"center"}
+                            justifyContent={"center"}
+                            bg="white"
+                            border={"1px solid"}
+                            borderColor={"gray.300"}
+                            borderRadius="xl"
+                            color="gray.500"
+                            width="80px"
+                            height="80px"
+                            fontSize="5xl"
+                        >
+                            💰
                         </Flex>
-                    </Stack>
-                </Box>
+                        <Heading size="2xl" textAlign="center">
+                            {expenseId ? t("payments.expense.edit-expense") : t("payments.expense.create-expense")}
+                        </Heading>
+                    </Flex>
 
-                <Flex justifyContent="space-between" gap={3}>
-                    <Button variant="outline" onClick={() => router.history.back()}>
-                        {t("cancel")}
-                    </Button>
-                    <Button colorScheme="blue" onClick={handleSave} disabled={buttonDisabled}>
-                        {t("save")}
-                    </Button>
-                    {/* {expenseId &&
+                    <Box p={6} borderWidth={1} borderRadius="xl" bg="white" h="full" display="flex" flexDirection={"column"}>
+                        <Stack gap={4} flexGrow={1}>
+                            <Box>
+                                <Text mb={1} fontWeight="bold">
+                                    {t("payments.expense.expense-description")}
+                                </Text>
+                                <Textarea
+                                    fontSize={"lg"}
+                                    // placeholder={t("payments.expense.expense-description")}
+                                    value={expenseDetails.description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                    resize={"none"}
+                                    variant={"flushed"}
+                                    alignContent={"end"}
+                                />
+                            </Box>
+
+                            <Box>
+                                <Text mb={1} fontWeight="bold">
+                                    {t("payments.expense.expense-amount")}
+                                </Text>
+                                <Flex alignItems="center" gap={2} position={"relative"}>
+                                    <NumberInput.Root
+
+                                        variant={"flushed"}
+                                        w={"full"}
+                                        onValueChange={(e) => setAmount(Number(e.value))}
+                                        value={String(expenseDetails.amount)}
+
+                                    >
+                                        {/* <NumberInput.Control /> */}
+                                        <NumberInput.Input placeholder="0.00" fontSize={"lg"} />
+                                    </NumberInput.Root>
+                                    <Text fontSize="2xl" position="absolute" left={2} top={1}>
+                                        ₪
+                                    </Text>
+                                </Flex>
+                            </Box>
+
+                            <Flex flexDirection={"column"} alignItems="center" gap={4} flexWrap="wrap" margin={"auto"} >
+                                <Text fontWeight="bold" fontSize={"sm"}>
+                                    {t("payments.expense.paid-by")}
+                                </Text>
+                                <SelectUserDrawer
+                                    size={"lg"}
+                                    title={t("payments.expense.select-paid-by")}
+                                    onSelect={setPaidBy}
+                                    trigger={
+                                        <Button size="2xl" px={4} py={1} h="unset" variant={"surface"}>
+                                            {expenseDetails.paidByUser?.firstName} {expenseDetails.paidByUser?.lastName}
+                                        </Button>
+                                    }
+                                />
+                                <Text fontWeight="bold" fontSize={"sm"}>
+                                    {t("payments.expense.and-splits")}
+                                </Text>
+                                <SplitDetailsDrawer
+                                    trigger={
+                                        <Button size="2xl" px={4} py={1} h="unset" variant={"surface"}>
+                                            {areSplitsValuesEqual ? t("payments.expense.equally") : t("payments.expense.unequally")}
+                                        </Button>
+                                    }
+                                />
+                            </Flex>
+                        </Stack>
+                        <Flex justifyContent="space-between" gap={3}>
+                            <Button variant="outline" onClick={() => router.history.back()} size={"lg"}>
+                                {t("cancel")}
+                            </Button>
+                            <Button colorScheme="blue" onClick={handleSave} disabled={buttonDisabled} size={"lg"}>
+                                {t("save")}
+                            </Button>
+                            {/* {expenseId &&
                         <Button colorScheme="red" variant="ghost" onClick={handleDelete}>
                             מחיקת הוצאה
                         </Button>
                     } */}
-                </Flex>
-            </Stack>
-        </Container >
+                        </Flex>
+                    </Box>
+
+                </Stack>
+            </Container >
+        </Box>
     );
 };
 
