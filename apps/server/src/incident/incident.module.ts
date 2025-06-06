accaf88db00ed4e480758c8910ffedbb95914a25
noammosh <noammosh@mobileye.com>
2025-06-06 13:49:13 +0300

    NotificationModule

--- apps/server/src/incident/incident.module.ts
+++ apps/server/src/incident/incident.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Incident, Comment } from './incident.entity';
 import { UserApartmentModule } from '../user-apartment/user-apartment.module';
 import { JwtModule } from '@nestjs/jwt';
+import { NotificationModule } from '../notification/notification.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Incident, Comment]),
     UserApartmentModule,
     JwtModule,
+    NotificationModule
   ],
   controllers: [IncidentController],
   providers: [IncidentService],
