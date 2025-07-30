ad6b363f06151ab587e7c4fcb8cb837304f19c12
noammosh <noammosh@mobileye.com>
2025-07-30 21:29:58 +0300

    linter

--- apps/client/src/routes/roommate/general-shopping-lists/$generalShoppingListId/items.tsx
+++ apps/client/src/routes/roommate/general-shopping-lists/$generalShoppingListId/items.tsx
@@ -2,14 +2,16 @@ import { createFileRoute } from '@tanstack/react-router';
 import GeneralShoppingListItemsPage from '../../../../pages/ShoppingLists/GeneralShoppingListItemsPage';
 import { GeneralShoppingListProvider } from '../../../../context/auth/GeneralShoppingListProvider';
 
+function GeneralShoppingListItemsRoute() {
+    const generalShoppingListId = Route.useParams().generalShoppingListId;
+    return (
+        <GeneralShoppingListProvider generalShoppingListId={generalShoppingListId}>
+            <GeneralShoppingListItemsPage />
+        </GeneralShoppingListProvider>
+    );
+}
+
 export const Route = createFileRoute('/roommate/general-shopping-lists/$generalShoppingListId/items')({
-    component: () => {
-        const generalShoppingListId = Route.useParams().generalShoppingListId;
-        return (
-            <GeneralShoppingListProvider generalShoppingListId={generalShoppingListId}>
-                <GeneralShoppingListItemsPage />
-            </GeneralShoppingListProvider>
-        );
-    }
+    component: GeneralShoppingListItemsRoute
 });
 
